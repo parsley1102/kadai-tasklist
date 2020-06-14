79c9428ce5bae6859b17507fac06f3b43d7b0b8f
nozomi.handa <parsley1102@gmail.com>
2020-06-14 12:10:54 +0000

    L15 kadai trial1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+  
+  before_action :require_user_logged_in
+  
   def index
     @tasks = Task.all
   end
@@ -12,14 +15,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'Taskを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Taskが投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'Taskの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 
@@ -44,7 +47,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   # Strong Parameter
