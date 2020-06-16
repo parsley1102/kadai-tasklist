34a1feebb99029dc80acaa58e09377d3c6d602f2
nozomi.handa <parsley1102@gmail.com>
2020-06-16 08:15:07 +0000

    4th? trial for kadai15

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user
   
   def index
     if logged_in?
@@ -9,47 +10,61 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    if logged_in?
+      @task = Task.find(params[:id])
+    end
   end
 
   def new
-    @task = Task.new
+    if logged_in?
+      @task = Task.new
+    end
   end
 
   def create
-    @task = current_user.tasks.build(task_params)
-    if @task.save
-      flash[:success] = 'Taskを投稿しました。'
-      redirect_to root_url
-    else
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      flash.now[:danger] = 'Taskの投稿に失敗しました。'
-      render 'tasks/new'
+    if logged_in?
+      @task = current_user.tasks.build(task_params)
+      if @task.save
+        flash[:success] = 'Taskを投稿しました。'
+        redirect_to root_url
+      else
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        flash.now[:danger] = 'Taskの投稿に失敗しました。'
+        render 'tasks/new'
+      end
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
+    
+    if logged_in?
+      @task = Task.find(params[:id])
+    end
   end
 
   def update
-    @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Taskは更新されませんでした'
-      render :edit
+    if logged_in?
+      @task = Task.find(params[:id])
+  
+      if @task.update(task_params)
+        flash[:success] = 'Taskは正常に更新されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Taskは更新されませんでした'
+        render :edit
+      end
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-
-    flash[:success] = 'Taskは正常に削除されました'
-    redirect_to root_url
+    if logged_in?
+      @task = Task.find(params[:id])
+      @task.destroy
+    
+    
+      flash[:success] = 'Taskは正常に削除されました'
+      redirect_to root_url
+    end
   end
   
   # Strong Parameter
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "toppages#index"
+  root to: "tasks#index"
   
   resources :tasks
   
