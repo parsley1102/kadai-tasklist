925cecaa9ff15acc1cbf7fbdf82f3d2586e24ae9
nozomi.handa <parsley1102@gmail.com>
2020-06-17 07:33:28 +0000

    7th tiral for kadail15

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,23 @@
 class TasksController < ApplicationController
   
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy, :edit, :update, :show]
   
   def index
-    if logged_in?
+
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
-    if logged_in?
       @task = Task.find(params[:id])
-    end
   end
 
   def new
-    if logged_in?
+
       @task = Task.new
-    end
   end
 
   def create
-    if logged_in?
       @task = current_user.tasks.build(task_params)
       if @task.save
         flash[:success] = 'Taskを投稿しました。'
@@ -32,18 +27,13 @@ class TasksController < ApplicationController
         flash.now[:danger] = 'Taskの投稿に失敗しました。'
         render 'tasks/new'
       end
-    end
   end
 
   def edit
-    
-    if logged_in?
       @task = Task.find(params[:id])
-    end
   end
 
   def update
-    if logged_in?
       @task = Task.find(params[:id])
   
       if @task.update(task_params)
@@ -53,18 +43,15 @@ class TasksController < ApplicationController
         flash.now[:danger] = 'Taskは更新されませんでした'
         render :edit
       end
-    end
   end
 
   def destroy
-    if logged_in?
       @task = Task.find(params[:id])
       @task.destroy
     
     
       flash[:success] = 'Taskは正常に削除されました'
       redirect_to root_url
-    end
   end
   
   # Strong Parameter
